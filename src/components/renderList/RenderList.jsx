fed8bdac9220ebf37d82e4c17bcfbd1a85c81bf0
unaben <unaben@yahoo.com>
2023-04-05 13:42:43 +0100

    [master] move folders

--- src/components/renderList/RenderList.jsx
+++ src/components/renderList/RenderList.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import List from "../../components/list/List";
 import { useDispatch, useSelector } from "react-redux/es/exports";
-import { getContacts } from "../features/contactSlice";
-import { allContacts } from "../features/contactSlice";
-import useFilter from "../Hooks/useFilter";
-import { deleteContact } from "../features/contactSlice";
+import { getContacts } from "../../features/contactSlice";
+import { allContacts } from "../../features/contactSlice";
+import useFilter from "../../Hooks/useFilter";
+import { deleteContact } from "../../features/contactSlice";
 
 const RenderList = () => {
   const dispatch = useDispatch();
